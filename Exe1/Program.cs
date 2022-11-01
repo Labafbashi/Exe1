e0921ec9d027c970daca5bf0299b437365cfd876
MohammadHossein Labafbashi <92016155+Labafbashi@users.noreply.github.com>
2022-11-01 15:57:11 +0100

    do-while

--- Exe1/Program.cs
+++ Exe1/Program.cs
@@ -2,6 +2,27 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Result =>");
+        string[] staffName = { "" };
+        int[] salary = { 0 };
+        int i = 0;
+
+
+        Console.WriteLine("=====>> Restaurangregistrering <<=====");
+        Console.WriteLine("\n\n");
+        do
+        {
+            Console.Write("Skriv namn: ");
+            staffName[i] = Console.ReadLine();
+            //if (staffName[i].ToUpper() == "END") { break; }
+            Console.Write("Skrive lön: ");
+            salary[i] = Convert.ToInt32(Console.ReadLine());
+            i++;
+        } while (staffName[i].ToUpper() != "END");
+
+        for(i = 0; i < salary.Length; i++)
+        {
+            Console.WriteLine($"Staff Name : {staffName[i]} === \t === Salary : {salary[i]}");
+
+        }
     }
 }
