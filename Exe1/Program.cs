56953f6a306dfae86c6795ec2856546408185b75
MohammadHossein Labafbashi <92016155+Labafbashi@users.noreply.github.com>
2022-11-01 22:56:52 +0100

    start to version 2 of application

--- Exe1/Program.cs
+++ Exe1/Program.cs
@@ -2,6 +2,7 @@
 {
     private static void Main(string[] args)
     {
+        /* This is version 1 of application
         List<string> staffName = new List<string>();
         List<int> salary = new List<int>();
         int i = -1;
@@ -26,6 +27,37 @@
         {
             Console.WriteLine($"{staffName[i]} \t \t \t {salary[i]}");
 
+        }*/
+        // Restaurant registration version 2
+        ShowMenu();
+        SelectMenu();
+
+    }
+
+    private static void SelectMenu()
+    {
+        Console.Write("Please Select a number from menu: ");
+        switch (Convert.ToInt32(Console.ReadLine()))
+        {
+            case 0:
+                //Exit application
+                break;
+            case 1:
+                //Personal registration
+                break;
+            case 2:
+                //Personal List
+                break;
         }
+    }
+
+    private static void ShowMenu()
+    {
+        Console.WriteLine("=====>> Restaurangregistrering <<===== \n\n\n");
+        Console.WriteLine("\t Main Menu");
+        Console.WriteLine("1. Registration");
+        Console.WriteLine("2. Show List");
+        Console.WriteLine("0. Exit");
+        Console.WriteLine("\n ======================================== \n \n");
     }
 }
